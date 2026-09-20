a89cc95258d978f040608d115c689bb1d48842bd
Aminnur <aminnur.ikhsan@gmail.com>
2026-09-20 09:07:35 +0700

    fix(api): satisfy phpstan on new api resources

--- app/Http/Resources/SubjectResource.php
+++ app/Http/Resources/SubjectResource.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Resources;
 
+use App\Models\BpsSubject;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @mixin BpsSubject
+ */
 class SubjectResource extends JsonResource
 {
     public function toArray(Request $request): array
